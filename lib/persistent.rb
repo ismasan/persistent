0be8a31862f4de3f45edd960f978b879816f7f0e
Ismael Celis <ismaelct@gmail.com>
2009-08-04 13:23:11 +0100

    Basic modules and spec for Attributes module

---
+++ lib/persistent.rb
@@ -0,0 +1,6 @@
+module Persistent
+  
+  autoload :Store,        'lib/persistent/store'
+  autoload :Persistence,  'lib/persistent/persistence'
+  autoload :Attributes,   'lib/persistent/attributes'
+end
---
+++ lib/persistent/store.rb
@@ -0,0 +1,31 @@
+require 'pstore'
+module Persistent
+  module Store
+    include Enumerable
+    
+    def initialize(key)
+      @store = PStore.new(key)
+    end
+    
+    def [](key)
+      @store.transaction{@store[key]}
+    end
+    
+    def []=(key, value)
+      @store.transaction{@store[key] = value}
+    end
+    
+    def fetch(key, default)
+      @store.transaction{@store.fetch(key, default)}
+    end
+    
+    def delete(key)
+      @store.transaction{@store.delete(key)}
+    end
+    
+    def each(&block)
+      roots = @store.transaction{@store.roots}
+      roots.each(&block)
+    end
+  end
+end
