aa09deb8b1d93ee216e1cfd9b7dada4e884d53ad
Ismael Celis <ismaelct@gmail.com>
2009-08-04 15:05:28 +0100

    BAsic spec for top level Persistent module

--- lib/persistent.rb
+++ lib/persistent.rb
@@ -1,5 +1,11 @@
 module Persistent
   
+  
+  def self.included(base)
+    base.send :include, Attributes
+    base.send :include, Persistence
+  end
+  
   autoload :Store,        'lib/persistent/store'
   autoload :Persistence,  'lib/persistent/persistence'
   autoload :Attributes,   'lib/persistent/attributes'
--- spec/persistent_spec.rb
+++ spec/persistent_spec.rb
@@ -1,5 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+module Persistent
+  class Test
+    include Persistent
+    
+    attribute :name, :last_name
+    
+    def initialize(id)
+      self.id = id
+      self.attributes = store.fetch(id, {})
+    end
+    
+  end
+end
+
 describe "Persistent" do
+  before do
+    Persistent::Test.store = Hash.new
+    @test = Persistent::Test.new('abc')
+  end
   
+  it "should have attributes" do
+    @test.respond_to?(:name).should be_true
+    @test.respond_to?(:last_name).should be_true
+  end
+  
+  it "should persist" do
+    @test.name = 'foo'
+    @test.save!
+    Persistent::Test.exists?('abc').should be_true
+    Persistent::Test.new('abc').should == @test
+  end
 end
